76376b38b383e3588476722c57d3f0c195fe14b2
srinivas1628 <sree.garikiparthy@gmail.com>
2021-04-23 21:14:00 +0530

    added Providersfile

---
+++ providers.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    features{}
+    client_id       ="3d075846-6a07-4315-9b6b-86216a484cc9"
+    client_secret   ="5RWW128K_IqXJghGi3qcgawfNgEa.2yYP4"
+    tenant_id       ="c9390984-1261-4e28-ac08-258d01591e3c"
+    subscription_id ="b4d4d430-dca0-4b8e-80b0-ce40e11e8039" 
+}
